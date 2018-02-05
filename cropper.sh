54ad8f286b0a93616e654484166b91e597a9bb29
pkarira <pulkit5208@gmail.com>
2018-02-05 18:51:13 +0530

    added copper shell script

---
+++ cropper.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+input="/home/pulkit/micrograph.txt"
+x=0;
+while IFS= read -r var
+do
+  i=${var##*,}
+  #echo $i
+  a[x]=$i
+  x=$((x+1))
+done < "$input"
+x=0;y=1;b[0]=0;b[1]=0;b[2]=0;b[3]=0;b[4]=0;b[5]=0;b[6]=0;
+for file in *.{tif,png}
+do	
+    convert -crop 644x484 "$file" cropped_%d.tif
+    rm -f cropped_1.tif
+    mv "cropped_0.tif" "$file"
+    convert -crop 322x242 "$file" cropped_%d.tif
+    rm -f $file	
+    if [[ ${a[$y]} == *"spheroidite+widmanstatten"* ]]
+    then
+     x=4
+    elif [[ ${a[$y]} == *"pearlite+spheroidite"* ]]
+    then
+     x=5
+    elif [[ ${a[$y]} == *"pearlite+widmanstatten"* ]]
+    then
+     x=6		
+    elif [[ ${a[$y]} == *"pearlite"* ]]
+    then
+     x=0
+    elif [[ ${a[$y]} == *"spheroidite"* ]]
+    then
+     x=1
+    elif [[ ${a[$y]} == *"martensite"* ]]
+    then
+     x=2
+    elif [[ ${a[$y]} == *"network"* ]]
+    then
+     x=3
+    fi
+    for file in cropped*
+    do 
+	mv "$file" "${a[$y]}_${b[$x]}.tif"
+	echo $x
+	b[$x]=$((b[$x] + 1))
+    done 
+    y=$((y + 1))				
+done
+
